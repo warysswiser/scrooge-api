79b24d5112429b62a9b148f6345822a2c47e418e
waryss <warysswiser@gmail.com>
2020-09-07 00:51:47 +0200

    set execution date format

--- src/main/java/com/warys/scrooge/domain/model/budget/CashFlow.java
+++ src/main/java/com/warys/scrooge/domain/model/budget/CashFlow.java
@@ -1,5 +1,6 @@
 package com.warys.scrooge.domain.model.budget;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.warys.scrooge.domain.model.GenericModel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class CashFlow extends GenericModel {
     private String label;
     private double amount;
     private String frequency;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate executionDate;
 
 }
